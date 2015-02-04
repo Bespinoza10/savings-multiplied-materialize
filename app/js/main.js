be5c9100123934e1845ece744e04fe65e770f269
Eric Brengle <eric.brengle@gmail.com>
2015-02-04 15:59:40 -0600

    added price

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@
 
   $.get( 'https://savingsmultipliedssh.firebaseio.com/items.json', function( data ) {
       _.forEach(data, function(item){
-        var item = $('<div class="col S3 productGrid"><div class = "image">' + '<img src =' + item.image + '>' + '</div><p class= "item-title">' + item.title + '</p><p class= "item-title">' + moment(item.endDate).fromNow() + '</p></div>');
+        var item = $('<div class="col S3 productGrid"><div class = "image">' + '<img src =' + item.image + '>' + '</div><p class= "item-title"> $' + item.price + '</p><p class= "item-title">' + item.title + '</p><p class= "item-title">' + moment(item.endDate).fromNow() + '</p></div>');
         $( '#product-grid' ).append( item );
 
       });
